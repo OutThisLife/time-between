a7a53338071c1e7f62080f8f53b8f56a7b621432
Talasan Nicholson <talasan.nicholson@gmail.com>
2018-11-06 19:28:03 -0600

    quick gal commit

--- app/pages/index.tsx
+++ app/pages/index.tsx
@@ -1,7 +1,9 @@
-import { defaultTheme, Pane, Text, TextInput } from 'evergreen-ui'
-import { KeyboardEventHandler } from 'react'
+import nlp from 'compromise'
+import dayjs from 'dayjs'
+import { defaultTheme, Icon, Pane, Text, TextInput } from 'evergreen-ui'
 import {
   compose,
+  onlyUpdateForKeys,
   setDisplayName,
   StateHandler,
   StateHandlerMap,
@@ -20,45 +22,84 @@ interface IStateHandlers extends StateHandlerMap<IState> {
 export default compose<IState & IStateHandlers, {}>(
   setDisplayName('index'),
   withStateHandlers(
-    {
-      result: ''
-    },
-
+    { result: '' },
     {
       getResult: () => ({
-        target: { value: result }
+        target: { value }
       }: React.ChangeEvent<HTMLInputElement>) => {
-        return { result }
+        localStorage.setItem('LAST_INPUT', value)
+
+        const doc: any[] = nlp(value)
+          .dates()
+          .data()
+
+        const dates = doc.reduce(
+          (acc, { date }) => acc.push(iter(date)) && acc,
+          []
+        )
+
+        if (dates.length === 2) {
+          return {
+            result: `${Math.abs(
+              dates[0].diff(dates[1], 'hour', true)
+            ).toLocaleString()} hours`
+          }
+        }
+
+        return { result: '' }
       }
     }
   ),
   withHandlers(() => ({
-    onRef: () => (ref: HTMLElement) => {
+    onRef: ({ getResult }) => (ref: HTMLElement) => {
       if (!ref) {
         return
       }
 
       const $input = ref.querySelector('input')
+      const lastInput = localStorage.getItem('LAST_INPUT')
 
       window.addEventListener(
         'keydown',
         () => document.activeElement !== $input && $input.focus()
       )
+
+      if (lastInput) {
+        $input.placeholder = lastInput
+        getResult({ target: { value: lastInput } })
+      }
     }
-  }))
+  })),
+  onlyUpdateForKeys(['result'])
 )(({ onRef, getResult, result }) => (
   <Pane
+    innerRef={onRef}
     display="flex"
     alignItems="center"
     justifyContent="center"
     minHeight="100vh"
     background={defaultTheme.scales.blue.B1}>
-    <Pane innerRef={onRef} width="100%" paddingX="2.5vmax">
+    <Pane is="form" width="100%" paddingX="2.5vmax">
+      <Icon
+        zIndex={0}
+        position="absolute"
+        top="50%"
+        left="50%"
+        transform="translate(-50%, -50%)"
+        icon="time"
+        size="70vw"
+        color={defaultTheme.scales.blue.B5}
+        opacity={0.08}
+      />
+
       <TextInput
+        zIndex={1}
+        position="relative"
         autoFocus
         tabIndex={1}
+        id="query"
         name="query"
-        placeholder="How many hours are there between yesterday and now?&hellip;"
+        placeholder="yesterday 5am and today 6pm"
         fontSize="2vmax"
         width="100%"
         height="auto"
@@ -73,16 +114,41 @@ export default compose<IState & IStateHandlers, {}>(
       />
 
       <Text
+        zIndex={1}
+        position="relative"
         is="h1"
         display="block"
-        color={defaultTheme.scales.blue.B4}
+        color={
+          result ? defaultTheme.scales.blue.B6 : defaultTheme.scales.blue.B4
+        }
         fontWeight={400}
         fontStyle="italic"
-        fontSize="2.5vmax"
+        fontSize="3vmax"
         textAlign="center"
-        textTransform="lowercase">
+        textTransform="lowercase"
+        transition="0.2s ease-in-out">
         {result || 'Waiting for query'}
       </Text>
     </Pane>
   </Pane>
 ))
+
+const iter = (obj: object, t = dayjs()) => {
+  const entries = Object.entries(obj).filter(
+    ([k, v]) => v && !['timezone', 'logic'].includes(k)
+  ) as Array<[dayjs.UnitType, number | string]>
+
+  for (const [k, v] of entries) {
+    if (v === 'yesterday') {
+      t = t.subtract(1, 'day')
+    } else if (v === 'tomorrow') {
+      t = t.add(1, 'day')
+    } else if (typeof v === 'number') {
+      t = t.set(k, v)
+    } else if (typeof v === 'object') {
+      t = iter(v, t)
+    }
+  }
+
+  return t
+}
